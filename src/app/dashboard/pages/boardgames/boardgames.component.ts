d757cf3e69d339ad52e80b05f7ba8739098b30dc
Franco Rodriguez <Franco.d.r1992@gmail.com>
2025-05-22 17:55:12 -0300

    search categori change with any bg search

--- src/app/dashboard/pages/boardgames/boardgames.component.ts
+++ src/app/dashboard/pages/boardgames/boardgames.component.ts
@@ -69,6 +69,7 @@ export class BoardgamesComponent implements OnInit, OnDestroy{
       ).subscribe(
         filteredBoardGames=>{
           this.boards = filteredBoardGames;
+          this.currentCategory = CategoryGame.ALL;
         }
       )
     }
